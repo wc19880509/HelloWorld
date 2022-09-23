a21031dfcebd56da4478210733fa5092077662cf
WangChen <wc_19880509@126.com>
2022-09-23 18:26:52 +0800

    pytest first commit

---
+++ test_abc.py
@@ -0,0 +1,26 @@
+#***********************************************
+#
+#      Filename: test_abc.py
+#
+#        Author: Chen Wanng 
+#          Mail: wc19880509@gmail.com
+#   Description: ---
+#        Create: 2022-09-23 18:26:14
+# Last Modified: 2022-09-23 18:26:14
+#***********************************************
+
+import pytest
+class Test_ABC:
+    def setup_class(self):
+        print("------->setup_class")
+
+    def teardown_class(self):
+        print("------->teardown_class")
+
+    def test_a(self):
+        print("------->test_a")
+        assert 1
+
+    def test_b(self):
+        print("------->test_b")
+        assert 0, "Error Image" # 断言失败```
